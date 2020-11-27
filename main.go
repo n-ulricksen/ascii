f9da921535b6fd044f36ceabc5812811ae6faf9f
Nicholas Ulricksen <n.ulricksen@gmail.com>
2020-11-27 14:32:13 -0800

    add -c flag for number of columns to print; clean up

--- main.go
+++ main.go
@@ -2,33 +2,34 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
+	"log"
 )
 
-var columns = 3
-var colorReset = "\033[0m"
-var colorCyan = "\033[36m"
+var columns int
+
+var (
+	columnHeader    = "Dec\tHex\tOct\tChar\t"
+	headerSeparator = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\t"
+	colorReset      = "\033[0m"
+	colorCyan       = "\033[36m"
+)
 
 func main() {
+	// Parse command line flags
+	flag.IntVar(&columns, "c", 3, "number of columns to print ASCII table")
+	flag.Parse()
+
+	if columns < 1 || columns > 5 {
+		log.Fatal("Invalid # of columns, use 1-5.")
+	}
+
 	var buf bytes.Buffer
 	var rows = 128/columns + 1
 
-	buf.WriteByte('\n')
-	for i := 0; i < columns; i++ {
-		buf.WriteString("Dec\tHex\tOct\tChar\t")
-		if i != columns-1 {
-			buf.WriteByte('|')
-			buf.WriteByte(' ')
-		}
-	}
-	buf.WriteByte('\n')
-	for i := 0; i < columns; i++ {
-		buf.WriteString("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\t")
-		if i != columns-1 {
-			buf.WriteByte('|')
-			buf.WriteByte(' ')
-		}
-	}
+	buf.WriteString(getColumnHeaders(columnHeader, columns))
+	buf.WriteString(getColumnHeaders(headerSeparator, columns))
 	buf.WriteByte('\n')
 
 	for row := 0; row < rows; row++ {
@@ -52,6 +53,22 @@ func main() {
 		}
 		buf.WriteByte('\n')
 	}
+	buf.WriteByte('\n')
 
 	fmt.Print(buf.String())
+}
+
+func getColumnHeaders(text string, columns int) string {
+	var buf bytes.Buffer
+
+	buf.WriteByte('\n')
+	for i := 0; i < columns; i++ {
+		buf.WriteString(text)
+		if i != columns-1 {
+			buf.WriteByte('|')
+			buf.WriteByte(' ')
+		}
+	}
+
+	return buf.String()
 }
